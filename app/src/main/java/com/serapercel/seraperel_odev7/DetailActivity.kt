5b582a50108b0f443599a744e9b8b260ae9c53cc
Serap <ercelserap@gmail.com>
2023-05-15 17:45:36 +0300

    Add delete note

--- app/src/main/java/com/serapercel/seraperel_odev7/DetailActivity.kt
+++ app/src/main/java/com/serapercel/seraperel_odev7/DetailActivity.kt
@@ -1,11 +1,42 @@
 package com.serapercel.seraperel_odev7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.serapercel.seraperel_odev7.database.DB
+import com.serapercel.seraperel_odev7.model.Note
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var db: DB
+    lateinit var tvTitle: TextView
+    lateinit var tvDetail: TextView
+    lateinit var tvDate: TextView
+    lateinit var btnDelete: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        db = DB(this)
+
+        tvTitle = findViewById(R.id.tvTitle)
+        tvDetail = findViewById(R.id.tvDetail)
+        tvDate = findViewById(R.id.tvDate)
+        btnDelete = findViewById(R.id.btnDelete)
+
+        val note= Note(MainActivity.thisNote!!.nid,MainActivity.thisNote!!.title,MainActivity.thisNote!!.detail,MainActivity.thisNote!!.date)
+        tvTitle.text = note.title
+        tvDetail.text = note.detail
+        tvDate.text = note.date
+
+        btnDelete.setOnClickListener {
+            db.deleteNote(note.nid)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
